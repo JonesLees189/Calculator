15e178cdfbcfcf5bc1b82ee55dc47b36bfab2f4a
Jones Lees <jbonesylees@gmail.com>
2025-04-20 10:56:42 -0700

    Added equal and percentage then changed some colors

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const percent = document.querySelector('.percent');
 const period = document.querySelector('.period');
 const sign = document.querySelector('.sign');
 const display = document.getElementById('numbers');
+const displayResult = document.getElementById('result');
+const displayEqual = document.getElementById('equal');
 const operatorSymbols = ['+', '-', '×', '÷'];
 const numberSymbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
@@ -77,8 +79,23 @@ function typeSign() {
 }
 function clearDisplay() {
     display.innerText = '0';
+    displayEqual.classList.add('d-none');
+    displayResult.classList.add('d-none');
 }
 
+function calculate() {
+    
+    if (operatorSymbols.includes(display.innerText[display.innerText.length - 1])) {
+        display.innerText = display.innerText.slice(0, -1);
+    }
+    displayInnerText = display.innerText.replace(/%/g, '/100');
+    let result = eval(displayInnerText.replace(/×/g, '*').replace(/÷/g, '/'));
+   
+    displayResult.innerText = result;
+    // remove class d-none from displayEqual
+    displayEqual.classList.remove('d-none');
+    displayResult.classList.remove('d-none');
+}
 numbers.forEach(number => {
     number.addEventListener('click', typeNumber);
 })
@@ -91,3 +108,9 @@ clear.addEventListener('click', clearDisplay);
 percent.addEventListener('click', typePercent);
 period.addEventListener('click', typePeriod);
 sign.addEventListener('click', typeSign);
+equal.addEventListener('click', calculate);
+display.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        calculate();
+    }
+})
